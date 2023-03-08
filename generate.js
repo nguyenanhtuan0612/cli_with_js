5cab0f254719717941e98f3e19e4ee3ffac542fa
dangdinhvan <untilyou1235@gmail.com>
2023-03-09 00:12:57 +0700

    add generate feature

---
+++ generate.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const featurePath = path.join(__dirname, 'src/features/feature1');
+
+const newFolderName = process.argv[2];
+
+const newFolderPath = path.join(__dirname, `src/features/${newFolderName}`);
+
+fs.mkdirSync(newFolderPath);
+
+fs.readdirSync(featurePath).forEach(folder => {
+	const childFolderPath = path.join(
+		__dirname,
+		`src/features/feature1/${folder}`
+	);
+
+	const newChildFolderPath = path.join(
+		__dirname,
+		`src/features/${newFolderName}/${folder}`
+	);
+
+	fs.mkdirSync(newChildFolderPath);
+
+	fs.readdirSync(childFolderPath).forEach(file => {
+		const sourceFilePath = path.join(childFolderPath, file);
+		const targetFilePath = path.join(newChildFolderPath, file);
+		fs.copyFileSync(sourceFilePath, targetFilePath);
+	});
+});
