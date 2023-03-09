1739928b9518febbcd1a766824c61391c9ae3550
tuananhvd1998 <tuananhvd1998@gmail.com>
2023-03-09 17:31:37 +0700

    validate

--- generate.js
+++ generate.js
@@ -8,6 +8,14 @@ const featurePath = path.join(__dirname, 'src/features/feature1');
 const newFolderName = process.argv[2];
 const folderPath = process.argv[3];
 
+if (!process.argv[2]) {
+  return console.log('foleder name is invalid!!');
+}
+
+if (!process.argv[3]) {
+  return console.log('folder path is invalid!!');
+}
+
 const newFolderPath = path.join(`${folderPath}/${newFolderName}`);
 
 if (fs.existsSync(newFolderPath)) {
