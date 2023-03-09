80eec9be3d0ed9326134227ae99a2edb2fe37667
tuananhvd1998 <tuananhvd1998@gmail.com>
2023-03-09 17:30:03 +0700

    commit

--- generate.js
+++ generate.js
@@ -6,27 +6,25 @@ const path = require('path');
 const featurePath = path.join(__dirname, 'src/features/feature1');
 
 const newFolderName = process.argv[2];
+const folderPath = process.argv[3];
 
-const newFolderPath = path.join(__dirname, `src/features/${newFolderName}`);
+const newFolderPath = path.join(`${folderPath}/${newFolderName}`);
 
+if (fs.existsSync(newFolderPath)) {
+  return;
+}
 fs.mkdirSync(newFolderPath);
 
 fs.readdirSync(featurePath).forEach(folder => {
-	const childFolderPath = path.join(
-		__dirname,
-		`src/features/feature1/${folder}`
-	);
+  const childFolderPath = path.join(__dirname, `src/features/feature1/${folder}`);
 
-	const newChildFolderPath = path.join(
-		__dirname,
-		`src/features/${newFolderName}/${folder}`
-	);
+  const newChildFolderPath = path.join(`${folderPath}/${newFolderName}/${folder}`);
 
-	fs.mkdirSync(newChildFolderPath);
+  fs.mkdirSync(newChildFolderPath);
 
-	fs.readdirSync(childFolderPath).forEach(file => {
-		const sourceFilePath = path.join(childFolderPath, file);
-		const targetFilePath = path.join(newChildFolderPath, file);
-		fs.copyFileSync(sourceFilePath, targetFilePath);
-	});
+  fs.readdirSync(childFolderPath).forEach(file => {
+    const sourceFilePath = path.join(childFolderPath, file);
+    const targetFilePath = path.join(newChildFolderPath, file);
+    fs.copyFileSync(sourceFilePath, targetFilePath);
+  });
 });
